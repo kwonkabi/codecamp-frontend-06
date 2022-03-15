9c660ac4a96dcf1f13acee0287daf47fe341b9b7
kwonkabi <seohyung0929@gmail.com>
2022-03-16 00:20:23 +0900

    프리보드 숙제 2일차

--- freeboard_frontend/freeboard/pages/boards/new/index.js
+++ freeboard_frontend/freeboard/pages/boards/new/index.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import {
   Wrapper,
   Title,
@@ -23,26 +25,90 @@ import {
 } from '../../../styles/emotion'
 
 export default function BoardsNewPage(){
+
+  const [writer, setWriter] = useState("");
+  const [password, setPassword] = useState("");
+  const [subject, setSubject] = useState("");
+  const [contents, setContents] = useState("");
+  const [address, setAddress] = useState("");
+  const [youtube, setYoutube] = useState("");
+
+  const [writerError, setWriterError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [subjectError, setSubjectError] = useState("");
+  const [contentError, setContentsError] = useState("");
+  const [addressError, setAddressError] = useState("");
+  const [youtubeError, setYoutubeError] = useState("");
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+    if (writer === "") {
+      setWriterError("이름을 적어주세요.");
+    } else {
+      setWriterError("");
+    }
+  };
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+    if (password === "") {
+      setPasswordError("비밀번호를 작성해주세요.");
+    } else {
+      setPasswordError("");
+    }
+  };
+  const onChangeSubject = (event) => {
+    setSubject(event.target.value);
+    if (subject === "") {
+      setSubjectError("제목을 작성해주세요.");
+    } else {
+      setSubjectError("");
+    }
+  };
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+    if (content === "") {
+      setContentsError("내용을 작성해주세요.");
+    } else {
+      setContentsError("");
+    }
+  };
+  const onChangeAddress = (event) => {
+    setAddress(event.target.value);
+    if (address === "") {
+      setAddressError("주소를 작성해주세요.");
+    } else {
+      setAddressError("");
+    }
+  };
+  const onChangeYoutube = (event) => {
+    setYoutube(event.target.value);
+    if (youtube === "") {
+      setYoutubeError("유튜브 링크를 작성해주세요.");
+    } else {
+      setYoutubeError("");
+    }
+  };
+
   return (
       <Wrapper>
       <Title>게시판 등록</Title>
       <WriterWrapper>
         <InputWrapper>
           <Label>작성자</Label>
-          <Writer type="text" placeholder="이름을 적어주세요." />
+          <Writer type="text" onChange={onChangeWriter} />
         </InputWrapper>
         <InputWrapper>
           <Label>비밀번호</Label>
-          <Password type="password" placeholder="비밀번호를 작성해주세요." />
+          <Password type="password" onChange={onChangePassword} />
         </InputWrapper>
       </WriterWrapper>
       <InputWrapper>
         <Label>제목</Label>
-        <Subject type="text" placeholder="제목을 작성해주세요." />
+        <Subject type="text" onChange={onChangeSubject} />
       </InputWrapper>
       <InputWrapper>
         <Label>내용</Label>
-        <Contents placeholder="내용을 작성해주세요." />
+        <Contents onChange={onChangeContents} />
       </InputWrapper>
       <InputWrapper>
         <Label>주소</Label>
@@ -50,12 +116,12 @@ export default function BoardsNewPage(){
           <Zipcode placeholder="07250" />
           <SearchButton>우편번호 검색</SearchButton>
         </ZipcodeWrapper>
-        <Address />
-        <Address />
+        <Address onChange={onChangeAddress}/>
+        <Address onChange={onChangeAddress}/>
       </InputWrapper>
       <InputWrapper>
         <Label>유튜브</Label>
-        <Youtube placeholder="링크를 복사해주세요." />
+        <Youtube onChange={onChangeYoutube} />
       </InputWrapper>
       <ImageWrapper>
         <Label>사진첨부</Label>
