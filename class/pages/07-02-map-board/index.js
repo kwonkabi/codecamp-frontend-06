30aee4e8c6a40c91874255e051279ae44b31c7e2
kwonkabi <seohyung0929@gmail.com>
2022-03-22 13:15:12 +0900

    key에 index가 들어가면 안 되는 이유

---
+++ class/pages/07-02-map-board/index.js
@@ -0,0 +1,51 @@
+// 05-04에서 복붙
+
+import { useQuery, gql } from '@apollo/client'
+import styled from '@emotion/styled'
+
+
+const FETCH_BOARDS = gql`
+  query fetchBoards{
+    fetchBoards{
+      number
+      writer
+      title
+      contents
+    }
+  }
+`
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const Column = styled.div`
+  width: 25%;
+`
+
+
+const MapBoardPage = () => {
+
+  const { data } = useQuery(FETCH_BOARDS)
+
+  return (
+    <>
+      {data?.fetchBoards.map((el) => (
+      // {data?.fetchBoards.map((el, index) => (
+        // 화면에 보이는 내용
+        <Row key={el.number}>
+          {/* 누가 누군지 모르니까 key에 서로 다른 값을 넣어주는 것! */}
+          {/* jsx니까 또 묶어주기 */}
+          <Column><input type="checkbox" /></Column>
+          <Column>{el.number}</Column> 
+          <Column>{el.title}</Column>
+          <Column>{el.writer}</Column>
+          {/* <Column>{el.writer}</Column> */}
+        </Row>
+      ))}
+    </>
+  )
+}
+
+export default MapBoardPage
