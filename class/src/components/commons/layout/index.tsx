5bd103af6c728028943fe73b06ac9eed6887610f
kwonkabi <seohyung0929@gmail.com>
2022-03-30 17:07:08 +0900

    레이아웃 만들기

---
+++ class/src/components/commons/layout/index.tsx
@@ -0,0 +1,52 @@
+import LayoutBanner from "./banner";
+import LayoutFooter from "./footer";
+import LayoutHeader from "./header";
+import LayoutNavigation from "./navigation";
+import styled from "@emotion/styled";
+import { ReactNode } from "react";
+import { useRouter } from "next/router";
+
+const BodyWrapper = styled.div`
+  display: flex;
+`;
+
+const Body = styled.div`
+  height: 300px;
+`;
+
+const LayoutSidebar = styled.div`
+  width: 100px;
+  height: 300px;
+  background-color: orange;
+`;
+
+const HIDDEN_HEADERS = [
+  "/12-05-modal-refactoring",
+  // ...
+  // ...
+  // ...
+];
+
+interface ILayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout(props: ILayoutProps) {
+  const router = useRouter();
+  console.log(router);
+
+  const isHidden = HIDDEN_HEADERS.includes(router.asPath);
+
+  return (
+    <>
+      {!isHidden && <LayoutHeader />}
+      <LayoutBanner />
+      <LayoutNavigation />
+      <BodyWrapper>
+        <LayoutSidebar>여기는 사이드바입니다!! SIDEBAR</LayoutSidebar>
+        <Body>{props.children}</Body>
+      </BodyWrapper>
+      <LayoutFooter />
+    </>
+  );
+}
