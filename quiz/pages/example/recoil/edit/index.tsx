efce3570bbbabcec314d1ded208bc588860a1e1c
kwonkabi <seohyung0929@gmail.com>
2022-04-11 17:10:55 +0900

    21st day quiz

--- quiz/pages/example/recoil/edit/index.tsx
+++ quiz/pages/example/recoil/edit/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
-import WritePage from "../../../../../src/components/units/example/write";
+import WritePage from "../../../../src/components/units/example/write";
 import { useRecoilState } from "recoil";
-import { isEditState } from "../../../../../src/commons/store";
+import { isEditState } from "../../../../src/commons/store";
 
 export default function EditPage() {
   const [isEdit, setIsEdit] = useRecoilState(isEditState);
